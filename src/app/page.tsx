ff3ef45baa5440cfae4c8e085461ced6e952e79d
Brain Costel <hugebrain711@gmail.com>
2025-05-13 16:31:38 -0600

    Update home page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { ComingSoon } from "@/components/ComingSoon";
+import { Lines } from "@/components/Lines";
 
 export default function Home() {
   return (
     <div className="flex h-screen w-screen">
       <main className="flex h-full w-full">
-          <ComingSoon 
-            title="Join the waitlist" 
-            subtitle="Inc."
-            tagLine="Comming soon"
-            siteUrl="http://issuesolved.ca/"
-            logoUrl="/logo.svg"
-            confirmationMsg="Thanks for interest! We'll be in touch soon."
+          <Lines 
+            title="ConsultantZero" 
+            subtitle="" 
+            tagLine=""
+            className="bg-black" 
             />
       </main>
     </div>
