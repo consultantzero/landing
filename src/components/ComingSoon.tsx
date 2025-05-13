051fa5aeb4a8a5113115e1fd5190011facaf80ce
franklinkemta <franklinkemta@gmail.com>
2025-05-13 07:32:50 -0600

    init template

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { BackgroundBeams } from "./ui/background-beams";
+
+export function ComingSoon() {
+  return (
+    <div className="h-full w-full rounded-none bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base min-h-30 sm:min-h-36 px-4 sm:px-5 sm:w-auto"
+            href="https://issuesolved.ca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/logo.svg"
+              alt="Website logo"
+              width={200}
+              height={200}
+            />
+            <span className="font-bold mt-4 text-base">Inc.</span>
+          </a>
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Join the waitlist
+        </h1>
+        <p></p>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-lg text-center relative z-10">
+          Comming soon
+        </p>
+        <input
+          type="text"
+          placeholder="you@orgnisation.com"
+          className="rounded-lg border text-lg p-2 text-neutral-50 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
+        />
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
